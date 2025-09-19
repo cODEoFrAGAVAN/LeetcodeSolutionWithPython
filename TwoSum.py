3face8e63ab19c3ee9f6fbe845e7e4a067213ef8
cODEoFrAGAVAN <postbox630003@gmail.com>
2025-09-19 13:41:32 +0530

    Second Commit

--- TwoSum.py
+++ TwoSum.py
@@ -9,13 +9,3 @@ class Solution:
             if diff in num_map:
                 return [num_map[diff], i]
             num_map[num] = i
-
-
-Length: int = int(input("Enter the length of the list\t"))
-TargetValue: int = int(input("Enter the target value\t"))
-InputList: list[int] = [
-    int(input("Enter the values\t")) for _ in range(0, Length)
-]
-obj1 = Solution()
-ReturnValue = obj1.twoSum(nums=InputList, target=TargetValue)
-print(ReturnValue)
